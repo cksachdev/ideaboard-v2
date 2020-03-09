8ac44528f2b5dfb97700afa384158266c7c14397
Nikunj Beladiya <nikunj.beladiya@tarento.com>
2020-03-09 16:24:35 +0530

    Added vue draggable to sort idea list

--- src/components/ListIdeas.vue
+++ src/components/ListIdeas.vue
@@ -2,27 +2,43 @@
     <v-row class="text-center" dense>
       <v-col cols="12">
         <v-expansion-panels v-if="GET_IDEA_LIST.length > 0" :accordion="true">
-          <v-expansion-panel v-for="(item, index) in GET_IDEA_LIST" :key="item.id">
-            <v-expansion-panel-header>{{ item.title }}</v-expansion-panel-header>
-            <v-expansion-panel-content>
-                <v-form v-model="formValid">
-                  <v-jsonschema-form 
-                  v-if="schema" 
-                  :schema="schema" 
-                  :model="GET_IDEA_LIST[index]"
-                  :options="options" 
-                  @error="showError" />
-                </v-form>
-                <v-card-actions right>
-                  <v-btn color="primary" :disabled="!formValid" @click="saveData(item.id, index)">Save</v-btn>
-                  <v-spacer></v-spacer>
-                  <v-btn color="red" icon @click="removeIdea(item.id)">
-                    <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </v-card-actions>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
+          <draggable draggable=".item" 
+                    v-model='myList'
+                    v-bind="dragOptions" 
+                    @start="drag = true" 
+                    @end="drag = false"
+                    :move="handleDrop"
+                    >
+            <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+              
+              <v-expansion-panel v-for="(item, index) in GET_IDEA_LIST" :key="item.id" class="item">
+                
+                <v-expansion-panel-header>
+                  <v-icon class="dragIcon">mdi-drag</v-icon>
+                  {{ item.title }}
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    <v-form v-model="formValid">
+                      <v-jsonschema-form 
+                      v-if="schema" 
+                      :schema="schema" 
+                      :model="GET_IDEA_LIST[index]"
+                      :options="options" 
+                      @error="showError" />
+                    </v-form>
+                    <v-card-actions right>
+                      <v-btn color="primary" :disabled="!formValid" @click="saveData(item.id, index)">Save</v-btn>
+                      <v-spacer></v-spacer>
+                      <v-btn color="red" icon @click="removeIdea(item.id)">
+                        <v-icon>mdi-delete</v-icon>
+                      </v-btn>
+                    </v-card-actions>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </transition-group>
+          </draggable>
         </v-expansion-panels>
+        
       </v-col>
     </v-row>
 </template>
@@ -30,11 +46,13 @@
 <script>
   import VJsonschemaForm from '@koumoul/vuetify-jsonschema-form/lib/index.vue'
   import FormSchema from '../FormSchema'
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters } from 'vuex' 
+import draggable from 'vuedraggable'
   export default {
     name: 'ListIdeas',
     components: {
-      VJsonschemaForm
+      VJsonschemaForm,
+      draggable
     },
     data: () => ({
       schema: FormSchema,
@@ -43,7 +61,8 @@ import { mapState, mapGetters } from 'vuex'
         debug: true,
         disableAll: false,
         autoFoldObjects: false
-      }
+      },
+      drag: false
     }),
     computed: {
       ...mapState([
@@ -51,7 +70,23 @@ import { mapState, mapGetters } from 'vuex'
       ]),
       ...mapGetters([
         'GET_IDEA_LIST'
-      ])
+      ]),
+      dragOptions() {
+        return {
+          animation: 200,
+          group: "description",
+          disabled: false,
+          ghostClass: "ghost"
+        };
+      },
+      myList: {
+          get() {
+              return this.GET_IDEA_LIST
+          },
+          set(value) {
+              this.$store.commit('SET_IDEA_TO_LIST', value)
+          }
+      }
     },
     mounted () {
       this.getIdeaLists();
@@ -75,7 +110,39 @@ import { mapState, mapGetters } from 'vuex'
         if(ideaId) {
           this.$store.dispatch('REMOVE_IDEA', {'ideaid': ideaId, 'showAlert': showAlert});
         }
+      },
+      handleDrop(evt) {
+        this.drag = false;
       }
     }
   }
 </script>
+
+<style lang="css">
+.v-expansion-panel-header {
+  padding-left: 30px !important;
+}
+.dragIcon {
+  position: absolute !important;
+  left: 0;
+}
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+.list-group {
+  min-height: 20px;
+}
+.list-group-item {
+  cursor: move;
+}
+.list-group-item i {
+  cursor: pointer;
+}
+</style>
